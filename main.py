b7b0d02211fef579794e2fbdc6742fdd05ddc3fb
dima-ku <54801704+dima-ku@users.noreply.github.com>
2021-04-20 20:49:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,78 @@
-from logging import debug
-from flask import Flask, request, redirect
-from flask.templating import render_template
-import json
-import meilisearch
-
-client = meilisearch.Client('http://127.0.0.1:8080')
-
-json_file = open('db/books.json')
-books = json.load(json_file)
-client.index('books').add_documents(books)
+from flask import Flask
+from data import db_session
+from data.users import User
+from flask import render_template
+from flask_wtf import FlaskForm
+from templates import *
+from flask_login import LoginManager, login_user, login_required
+from forms.user import RegisterForm
+from forms.user import LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-    
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
+@app.route('/index')
+@app.route('/register', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
+@login_manager.user_loader
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+# -_-
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
 def index():
-    if request.method == 'POST':
-        search_request = request.form['search_bar']
-        return redirect(f'/{search_request}')
-    param = {}
-    param['title'] = 'Лучший сайт по поиску книг'
-    return render_template('base.html')
+    user = "dima"
+    return render_template('index.html', title='Домашняя страница',
+                           username=user)
 
 
-@app.route('/<search_request>')
-def search_result(search_request):
-    param = {}
-    param['title'] = search_request
-    param['books'] = client.index('books').search(search_request)['hits']
-    return render_template('search_result.html', param=param)
+def main():
+    db_session.global_init("db/users_db.db")
+    app.run()
 
 
-app.run(debug=True)
+if __name__ == '__main__':
+    main()
