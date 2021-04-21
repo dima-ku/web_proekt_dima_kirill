f3626f1b1ab1824ca47a64576ced8f7f21cdf9ef
grdkrll <grdkrll@gmail.com>
2021-04-21 18:15:48 +0300

    added book page📚

--- main.py
+++ main.py
@@ -19,18 +19,25 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 def index():
     if request.method == 'POST':
         search_request = request.form['search_bar']
-        return redirect(f'/{search_request}')
+        return redirect(f'/search/{search_request}')
     param = {}
     param['title'] = 'Лучший сайт по поиску книг'
     return render_template('base.html')
 
 
-@app.route('/<search_request>')
+@app.route('/search/<search_request>')
 def search_result(search_request):
     param = {}
     param['title'] = search_request
     param['books'] = client.index('books').search(search_request)['hits']
     return render_template('search_result.html', param=param)
+
+@app.route('/books/<book_id>')
+def book_page(book_id):
+    param = {}
+    param['book'] = client.index('books').search(book_id)['hits'][0]
+    param['title'] = param['book']['title']
+    return render_template('book.html', param=param)
 
 
 app.run(debug=True)
